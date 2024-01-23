e9ad3476f7c5ce12a9560edf801a69d9c17b524d
Gonçalo Marques <m4rques1s0n@gmail.com>
2024-01-23 21:22:30 +0000

    up

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const sqlite3 = require("sqlite3").verbose();
 
 const INSERT_QUERY = "INSERT INTO marcacoes (email, user_number, service, duration, ano, mes, dia, hora, minuto) VALUES(?,?,?,?,?,?,?,?,?)";
-const SELECT_ALL_QUERY = "SELECT ano,mes,dia,hora,minuto,duration FROM marcacoes";
+const SELECT_ALL_QUERY = "SELECT email, user_number, service, ano,mes,dia,hora,minuto,duration FROM marcacoes";
 const SELECT_ALL_QUERY_PRODUCTS = "SELECT name,price,image,duration FROM products";
 const SELECT_ALLFOR_SMS_QUERY = "SELECT user_number, ano,mes,dia,hora,minuto,duration FROM marcacoes";
 const SELECT_PRODUCT_QUERY = "SELECT * FROM products WHERE name=?";
@@ -9,128 +9,128 @@ const INSERT_QUERY_CREATE_PRODUCT = "INSERT INTO products (name,price,image,dura
 const DELETE_PRODUCT_QUERY = "DELETE FROM products WHERE name = ?";
 
 const db = new sqlite3.Database("../backend/base.db", sqlite3.OPEN_READWRITE, (err) => {
-    if (err) return console.log(err.message);
+  if (err) return console.log(err.message);
 });
 
 function add_db(service, email, user_number, ano, mes, dia, hora, minuto, duration) {
-    return new Promise((resolve, reject) => {
-        db.run(INSERT_QUERY, [email, user_number, service, duration, ano, mes, dia, hora, minuto], (err) => {
-            if (err) {
-                reject(err);
-            }
-            resolve();
-        });
+  return new Promise((resolve, reject) => {
+    db.run(INSERT_QUERY, [email, user_number, service, duration, ano, mes, dia, hora, minuto], (err) => {
+      if (err) {
+        reject(err);
+      }
+      resolve();
     });
+  });
 }
 
 function read_db() {
-    return new Promise((resolve, reject) => {
-        db.all(SELECT_ALL_QUERY, function (err, data) {
-            if (err) {
-                reject(err);
-            }
-            resolve(data);
-        });
+  return new Promise((resolve, reject) => {
+    db.all(SELECT_ALL_QUERY, function (err, data) {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
     });
+  });
 }
 
 function read_db_products() {
-    return new Promise((resolve, reject) => {
-        db.all(SELECT_ALL_QUERY_PRODUCTS, function (err, data) {
-            if (err) {
-                reject(err);
-            }
-            resolve(data);
-        });
+  return new Promise((resolve, reject) => {
+    db.all(SELECT_ALL_QUERY_PRODUCTS, function (err, data) {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
     });
+  });
 }
 
 function read_db_sms() {
-    return new Promise((resolve, reject) => {
-        db.all(SELECT_ALLFOR_SMS_QUERY, function (err, data) {
-            if (err) {
-                reject(err);
-            }
-            resolve(data);
-        });
+  return new Promise((resolve, reject) => {
+    db.all(SELECT_ALLFOR_SMS_QUERY, function (err, data) {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
     });
+  });
 }
 
 function get_product_on_db(name) {
-    return new Promise((resolve, reject) => {
-        db.all(SELECT_PRODUCT_QUERY, [name], function (err, data) {
-            if (err) {
-                reject(err);
-            }
-            resolve(data);
-        });
+  return new Promise((resolve, reject) => {
+    db.all(SELECT_PRODUCT_QUERY, [name], function (err, data) {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
     });
+  });
 }
 
 function create_new_product_on_db(name, price, image, duration) {
-    return new Promise((resolve, reject) => {
-        db.run(INSERT_QUERY_CREATE_PRODUCT, [name, price, image, duration], (err) => {
-            if (err) {
-                reject(err);
-            }
-            resolve();
-        });
+  return new Promise((resolve, reject) => {
+    db.run(INSERT_QUERY_CREATE_PRODUCT, [name, price, image, duration], (err) => {
+      if (err) {
+        reject(err);
+      }
+      resolve();
     });
+  });
 }
 
 function delete_product_on_db(product) {
-    return new Promise((resolve, reject) => {
-        db.run(DELETE_PRODUCT_QUERY, [product], (err, data) => {
-            if (err) {
-                reject(err);
-            }
-            resolve();
-        });
+  return new Promise((resolve, reject) => {
+    db.run(DELETE_PRODUCT_QUERY, [product], (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve();
     });
+  });
 }
 
 function add_user(user, password) {
-    return new Promise((resolve, reject) => {
-        db.run("INSERT INTO users (user,password) VALUES(?,?)", [user, password], (err) => {
-            if (err) {
-                reject(err);
-            }
-            resolve();
-        });
+  return new Promise((resolve, reject) => {
+    db.run("INSERT INTO users (user,password) VALUES(?,?)", [user, password], (err) => {
+      if (err) {
+        reject(err);
+      }
+      resolve();
     });
+  });
 }
 
 function delete_user(user) {
-    return new Promise((resolve, reject) => {
-        db.run("DELETE FROM users WHERE user = ?", [user], (err) => {
-            if (err) {
-                reject(err);
-            }
-            resolve();
-        });
+  return new Promise((resolve, reject) => {
+    db.run("DELETE FROM users WHERE user = ?", [user], (err) => {
+      if (err) {
+        reject(err);
+      }
+      resolve();
     });
+  });
 }
 
 function search_for_user(user) {
-    return new Promise((resolve, reject) => {
-        db.all("SELECT * FROM users WHERE user=?", [user], function (err, data) {
-            if (err) {
-                reject(err);
-            }
-            resolve(data);
-        });
+  return new Promise((resolve, reject) => {
+    db.all("SELECT * FROM users WHERE user=?", [user], function (err, data) {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
     });
+  });
 }
 
 module.exports = {
-    add_db,
-    read_db,
-    get_product_on_db,
-    read_db_sms,
-    create_new_product_on_db,
-    search_for_user,
-    read_db_products,
-    delete_product_on_db,
-    add_user,
-    delete_user,
+  add_db,
+  read_db,
+  get_product_on_db,
+  read_db_sms,
+  create_new_product_on_db,
+  search_for_user,
+  read_db_products,
+  delete_product_on_db,
+  add_user,
+  delete_user,
 };
